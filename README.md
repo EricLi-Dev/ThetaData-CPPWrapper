67ba4f677b297723670a2730fbf1a3ce17898329
EricLi-Dev <johnlee888123456@gmail.com>
2023-10-18 20:31:41 -0400

    thetaData test API endpoint

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Description
 C++ Wrapper for ThetaData REST API endpoints
 
+## Run ThetaTerminal
+[Install ThetaTerminal.jar](https://http-docs.thetadata.us/docs/thetadata-rest-api/9nor5z0im37mq-getting-started)
+```
+java -jar ThetaTerminal.jar <username> <password>
+```
+
 ## Build
 ```
 cmake . 
--- main.cpp
+++ main.cpp
@@ -16,14 +16,23 @@ int main(void)
   std::string readBuffer;
 
   curl = curl_easy_init();
+  //std::string *url = "http://127.0.0.1:25510/hist/stock/eod?root=AAPL&start_date=20220901&end_date=20220915";
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:25510/hist/stock/eod?root=AAPL&start_date=20220901&end_date=20220915");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Accept: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
     res = curl_easy_perform(curl);
+    //curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+    //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    //curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    //res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
-    std::cout << readBuffer << std::endl;
+    //std::cout << readBuffer << std::endl;
   }
   return 0;
 }
