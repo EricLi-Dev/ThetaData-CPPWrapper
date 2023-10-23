8bcfdbe8e42d0bc2a0ed533f0b2fe1d3885f89ac
EricLi-Dev <johnlee888123456@gmail.com>
2023-10-23 19:51:32 -0400

    using same dataBuffer

--- main.cpp
+++ main.cpp
@@ -9,12 +9,10 @@ int main(void)
     ThetaClient client = ThetaClient("", "");
     int res;
     std::string dataBuffer;
-    std::string data2Buffer;
+    res = client.list_roots("Option", dataBuffer);
     res = client.get_hist_stock("EOD", "AAPL", "20220901", "20220915", dataBuffer);
-    res = client.get_hist_option("EOD", "AAPL", "20220930", 150, "C", "20220901", "20220915", data2Buffer);
-    res = client.list_roots("Option", data2Buffer);
+    res = client.get_hist_option("EOD", "AAPL", "20220930", 150, "C", "20220901", "20220915", dataBuffer);
 
     std::cout << dataBuffer << std::endl;
-    std::cout << data2Buffer << std::endl;
     return 0;
 }
